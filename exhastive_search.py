3482cb487eafe0c27fd0bcf60f03d689d361f06a
say3no <say3no@gmail.com>
2018-08-25 18:02:14 +0900

    w

--- exhastive_search.py
+++ exhastive_search.py
@@ -1,13 +1,15 @@
 # vim: set fileencoding=utf-8:
 def main():
-    b = [0] * 10
+    b = [0] * 4
     sim_bin(b)
 
 
 def sim_bin(b):
+    if i - 1 != len(b):
+        sim_bin(b, i)
+
+    b[i] = 1
     print(b)
-    if len(b) != 1:
-        sim_bin(b[1:])
 
 
 if __name__ == "__main__":
