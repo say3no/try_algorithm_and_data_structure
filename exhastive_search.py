6ab0ac9ac34bbfbbe33b865c8a610ce9fea9d3c0
say3no <say3no@gmail.com>
2018-08-25 18:35:06 +0900

    fuga

--- exhastive_search.py
+++ exhastive_search.py
@@ -1,15 +1,16 @@
 # vim: set fileencoding=utf-8:
 def main():
-    b = [0] * 4
-    sim_bin(b)
+    b = [0] * 5
+    sim_bin(b, 0, len(b))
 
 
-def sim_bin(b):
-    if i - 1 != len(b):
-        sim_bin(b, i)
-
-    b[i] = 1
-    print(b)
+def sim_bin(b, i, j):
+    #  重複がでてくる。なんでだ？？？？？？？？？？？
+    if i + 1 <= j:
+        b[i] = 0
+        sim_bin(b, i + 1, j)
+        b[i] = 1
+        sim_bin(b, i + 1, j)
 
 
 if __name__ == "__main__":
